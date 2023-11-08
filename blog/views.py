1ffcd4a943d3b95b1fd25cafe4e58e32fbf0d1a9
Sam Ferguson <sam@thirstyit.com.au>
2023-11-09 00:01:18 +1100

    added db entries, styling to blog front page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render( request, 'blog/index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render( request, 'blog/index.html', context)
+
